8d83768b3b504f3b97786cb1684184da2e09a604
David Estrada <david.estrada@huskers.unl.edu>
2020-04-15 13:11:55 -0500

    Started the Student Side menu
    Issue #2

--- src/main/java/edu/unl/cse/csce361/course_scheduler/frontend/CLI.java
+++ src/main/java/edu/unl/cse/csce361/course_scheduler/frontend/CLI.java
@@ -1,5 +1,8 @@
 package edu.unl.cse.csce361.course_scheduler.frontend;
 
+import edu.unl.cse.csce361.course_scheduler.Backend.Student;
+import edu.unl.cse.csce361.course_scheduler.Backend.User;
+
 import java.awt.*;
 import java.util.InputMismatchException;
 import java.util.NoSuchElementException;
@@ -45,7 +48,40 @@ public class CLI {
                     System.out.println("Admin login has not yet been implemented");
                     break;
                 case STUDENT_SIDE:
-                    System.out.println("Student login has not yet been implemented");
+                    System.out.println("Student Portal: ");
+                    System.out.println("Please enter student id: ");
+                    String studentId = scanner.nextLine();
+
+                    //Check to see if a student exists with this studentId
+                    User student = Student.getStudent(studentId);
+                    if(student == null) {
+                        System.out.println("No student record with this student id, try again or create record: ");
+                        System.out.println("1 - Enter Student Id again");
+                        System.out.println("2 - Create student record");
+                        //Back to main menu prompt
+                        System.out.println();
+                        System.out.println("Please select an option: ");
+                        int studentOption = scanner.nextInt();
+                        if(studentOption == 1) {
+                            //Run the STUDENT_SIDE case again
+                        } else if(studentOption == 2) {
+                            System.out.println("Creating Student record");
+                            System.out.println();
+                            System.out.print("Please enter your name: ");
+                            scanner.nextLine();
+                            String name = scanner.nextLine();
+                            System.out.print("Please enter your grade level: ");
+                            String gradeLevel = scanner.nextLine();
+                            //Create new student object
+                            //User newStudent = new Student(name, studentId, gradeLevel);
+                        }
+                    } else {
+                        System.out.println("Not yet implemented");
+                        //Welcome message
+                        //Prompt the user to enter an option of either editing schedule or entering schedule
+                    }
+
+
                     break;
                 case EXIT:
                     System.out.println("Goodbye!");
