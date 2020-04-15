de5504d07222d7221cfe111d5b73ed45cd21e11a
David Estrada <david.estrada@huskers.unl.edu>
2020-04-15 17:33:32 -0500

    Adapted StudentImpl class to Student class and extend abstract User
    Issue #2

--- src/main/java/edu/unl/cse/csce361/course_scheduler/Backend/Student.java
+++ src/main/java/edu/unl/cse/csce361/course_scheduler/Backend/Student.java
@@ -1,7 +1,40 @@
 package edu.unl.cse.csce361.course_scheduler.Backend;
 
-public interface Student {
-    String getName();
-    String getStudentId();
-    String getGradeLevel();
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Student extends User {
+    private String name;
+    private String studentId;
+    private String gradeLevel;
+    private static ArrayList<Student> students = new ArrayList<Student>();
+
+
+    public Student(String name, String studentId, String gradeLevel) {
+        super();
+        this.name = name;
+        this.studentId = studentId;
+        this.gradeLevel = gradeLevel;
+        students.add(this);
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getId() {
+        return studentId;
+    }
+
+    public String getGradeLevel() {
+        return gradeLevel;
+    }
+
+    @Override
+    public String toString() {
+        return "User: Student, Name: " + name + ", Student Id: " + studentId + ", Grade Level: " + gradeLevel;
+    }
+
 }
