2cbe9af243b3a67c1293d71dcd296ed88c821c9d
David Estrada <david.estrada@huskers.unl.edu>
2020-04-16 23:57:13 -0500

    Fixed writeToFile method to take Student object as parameter

--- src/main/java/edu/unl/cse/csce361/course_scheduler/Backend/CsvWriter.java
+++ src/main/java/edu/unl/cse/csce361/course_scheduler/Backend/CsvWriter.java
@@ -6,12 +6,16 @@ import java.io.IOException;
 
 public class CsvWriter {
 
-    public void writeToFile(String fileName, String newLine) {
+    public void writeToFile(String fileName, Student student) {
         fileName = "src/main/resources/csv/" + fileName;
 
         try {
             FileWriter filewriter = new FileWriter(fileName, true);
             BufferedWriter out = new BufferedWriter(filewriter);
+            String name = student.getName();
+            String id = student.getId();
+            String gradeLevel = student.getGradeLevel();
+            String newLine = name + "," + id + "," + gradeLevel;
             out.append("\n" + newLine);
             out.flush();
             out.close();
