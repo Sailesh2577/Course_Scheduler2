3ba0f974e3ad39b7986de779c07d47b966806b9f
Sailesh Pujara <spujara@huskers.unl.edu>
2020-05-01 00:56:17 -0500

    Resolved merge conflicts

--- src/main/java/edu/unl/cse/csce361/course_scheduler/backend/Course.java
+++ src/main/java/edu/unl/cse/csce361/course_scheduler/backend/Course.java
@@ -1,33 +1,7 @@
 package edu.unl.cse.csce361.course_scheduler.backend;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-
 public abstract class Course {
-    private String courseName;
-    private String courseNumber;
-    private static ArrayList<Course> courses = new ArrayList<>();
-
-    //Constructor
-    public Course(String courseName, String courseNumber) {
-        this.courseName = courseName;
-        this.courseNumber = courseNumber;
-        courses.add(this);
-    }
-
-    public static Collection<Course> setAllCourses(Collection<Map<String, String>> courseList) {
-        List<Course> courses = new ArrayList<Course>();
-
-        for (Map<String,String> map : courseList) {
-            courses.add(new Course(map.get("Name"), map.get("Course#")) {
-            });
-        }
-        return courses;
-    }
-
-    public abstract String getCourseName(){ };
-    public abstract String getCourseNumber(){ }
-
+    public abstract String getCourseName();
+    public abstract String getCourseNumber();
 }
+
